b2d3b11c8b6b90d0f9fdc852e2c3adcca6f39ec3
primaam <pmaharyono@gmail.com>
2024-05-28 17:51:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { fakeData } from "./util/FakeData";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isLoading, setIsLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+    }, []);
+
+    // id, name, username, email, city, dan website
+    const columns = [
+        { field: "id", headerName: "ID", width: 40 },
+        { field: "name", headerName: "Name", width: 100 },
+        { field: "username", headerName: "Username", width: 100 },
+        { field: "email", headerName: "Email", width: 100 },
+        { field: "city", headerName: "City", width: 100 },
+        { field: "website", headerName: "Website", width: 100 },
+    ];
+
+    return (
+        <div className="App">
+            {isLoading == false ? (
+                <div>test</div>
+            ) : (
+                <Box
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                        height: "70vh",
+                    }}
+                >
+                    <CircularProgress color="primary" size={80} />
+                    <br />
+                    <br />
+                    <Typography
+                        style={{
+                            fontSize: 30,
+                            color: "#fff",
+                        }}
+                    >
+                        Please wait while fetching a new data
+                    </Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
 export default App;
